fe2abf27d465390dee150559c342f323a3028cf5
suvinkumarselvaraj <vsky987@gmail.com>
2022-03-16 10:08:27 +0530

    add extra components and routes

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import './App.css';
 import Header from './Header';
 import Home from './Home';
 import Footer from './Footer';
-import MovieCards from './MovieCards';
-import DisplaySeats from './DisplaySeats';
+
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import CallCards from './CallCards';
+import ValimaiBooking from './ValimaiBooking';
 
 function App() {
 
@@ -14,28 +14,23 @@ function App() {
     <Router>
     <div className="App">
       <Switch>  
-          <Route path = "/header">
-            <Header/>
-          </Route>
+          
           <Route path = "/movies/valimai/1">
             <Header />
             <CallCards id ={1}/>
             <Footer></Footer>
           </Route>  
-          <Route path = "/movies/etharkumthuninthavan/2">
-            <Header />
-          </Route>  
+          
           <Route path = "/movies/1/valimai/book_now">
             <Header />
-              <DisplaySeats />
-            <Footer></Footer>
+              <ValimaiBooking />
           </Route>
           <Route path = "/">
           <Header></Header>
           <Home />
           <Footer></Footer>
           </Route>
-          
+
           </Switch>
     </div>
     </Router>
